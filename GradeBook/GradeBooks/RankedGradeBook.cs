69f3384d7e9427cdb4a511132960e43cc6720c60
Mateusz M <61331751+Informator4@users.noreply.github.com>
2023-04-03 16:14:37 +0200

    Zadanie: (1.6.1)
    CalculateStatistics (override method)

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,18 +15,13 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-            {
                 throw new InvalidOperationException();
-            }
+
             int b = 0;
 
             foreach (var student in Students)
-            {
                 if (averageGrade > student.AverageGrade)
-                {
                     b++;
-                }
-            }
 
             if (b >= 0.8 * Students.Count)
                 return 'A';
@@ -38,6 +33,14 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
         }
     }
 }
