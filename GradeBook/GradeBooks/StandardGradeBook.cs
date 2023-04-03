633113f6d6011110f3d6257fd9b8077ef2ce6275
Mateusz M <61331751+Informator4@users.noreply.github.com>
2023-04-03 13:05:23 +0200

    Drugie zadanie
    Zrobione

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,19 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+
+        public StandardGradeBook(string name) : base(name)
+        {
+
+            Type = GradeBookType.Standard;
+        }
+
+        public GradeBookType Type { get; }
+    }
+}
