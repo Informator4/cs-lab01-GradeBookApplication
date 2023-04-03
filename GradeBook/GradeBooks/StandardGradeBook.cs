0d37bd9021f50dce641250210142081633ce999e
Mateusz M <61331751+Informator4@users.noreply.github.com>
2023-04-03 13:13:15 +0200

    Drugie i trzecie zadanie
    Poprawa drugiego zadania

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -7,13 +7,10 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-
         public StandardGradeBook(string name) : base(name)
         {
 
             Type = GradeBookType.Standard;
         }
-
-        public GradeBookType Type { get; }
     }
 }
